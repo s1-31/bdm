43cee25a3c0600663e20bc4e152291593f590b21
Nisho Matsushita <robonich@gmail.com>
2015-12-04 01:58:57 +0000

    change size

--- timer.py
+++ timer.py
@@ -13,7 +13,7 @@ class Timer:
         #ウィンドウを作成
         self.window = gtk.Window()
         self.window.set_border_width(5)
-        self.window.set_size_request(400, 300)
+        self.window.set_size_request(200, 150)
         self.window.set_title('BDM')
         self.window.connect('destroy_event', self.end_application)
         self.window.connect('delete_event', self.end_application)
