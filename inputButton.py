5f8b023611353e3d82e778c8b426b96267928631
Nisho Matsushita <robonich@gmail.com>
2015-12-04 02:36:31 +0000

    add detect button input

---
+++ inputButton.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import commands
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    input_state = GPIO.input(18)
+    if input_state == False:
+	print(commands.getstatusoutput('xstroke'))
+        # print('Button Pressed')
+        time.sleep(0.2)
