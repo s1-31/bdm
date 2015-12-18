df49e39d287e3b76a42f71a75eaabf21633ee03e
Nisho Matsushita <robonich@gmail.com>
2015-12-18 13:19:58 +0900

    raspi微調整

--- testGPIO.py
+++ testGPIO.py
@@ -5,7 +5,7 @@ import time
 import subprocess
 
 IN_NO = 26
-OUT_NO = 20
+OUT_NO = 21
 
 print "press ^C to exit program ...\n"
 
@@ -15,13 +15,29 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(IN_NO, GPIO.IN)
 GPIO.setup(OUT_NO, GPIO.OUT)
 
-process = subprocess.Popen('python while.py')
+n=0
 
 try:
     while True:
         GPIO.output(OUT_NO, True)
-        if GPIO.input(IN_NO):
-            print process.poll()
+	if GPIO.input(IN_NO):
+	    if n == 0:
+		process = subprocess.Popen('toggle-timer.sh')
+		print 'OK'
+		n+=1
+	    elif n == 2:
+		process = subprocess.Popen('sudo killall python')
+		#process = subprocess.Popen('toggle-timer.sh')
+		print 'Close'
+		n+=1
+	else:
+	    if n == 1:
+		n+=1
+	    elif n == 3:
+		n = 0
+	#print GPIO.input(IN_NO)
+        #if GPIO.input(IN_NO):
+            #print process.poll()
 
 except KeyboardInterrupt:
             print "detect key interrupt\n"
